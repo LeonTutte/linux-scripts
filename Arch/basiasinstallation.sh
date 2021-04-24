70e5a1b31db9e0b04f30ca4f86fe0d2cf716aad3
Leon Tutte <leonacc@ltutte.de>
2021-04-24 09:32:36 +0000

    Sektion zur Aktualisierung der Spiegelserver und des Betriebssystems hinzugefügt.

--- Arch/basiasinstallation.sh
+++ Arch/basiasinstallation.sh
@@ -21,3 +21,11 @@ then
 	mkdir ~/git
     echo "Gitverzeichnis wurde erstellt"
 fi
+# Aktualisierung des Betriebsystems
+echo "Aktualisiere Spiegelserver"
+doas cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+curl -s "https://archlinux.org/mirrorlist/?country=FR&country=GB&protocol=https&use_mirror_status=on" -o ~/mirrorlist_new
+sed 's/#Server/Server/g' ~/mirrorlist_new >> ~/mirrorlist_new
+doas mv ~/mirrorlist_new /etc/pacman.d/mirrorlist
+echo "Suche nach Updates"
+doas pacman -Syyu
