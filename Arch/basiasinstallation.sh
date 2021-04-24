f704f79d692339b64887b9d2f6d4fbbb6503e1fb
Leon Tutte <leonacc@ltutte.de>
2021-04-24 08:59:50 +0000

    Beschreibung und Abhängigkeiten hinzugefügt.

--- Arch/basiasinstallation.sh
+++ Arch/basiasinstallation.sh
@@ -4,3 +4,10 @@
 #			(____)(__)
 #
 # Leon Tutte 
+#
+# Beschreibung
+# Dieses Skript setzt eine funktionierende Internetverbindung und einen "normalen" Benutzer voraus.
+#
+# Abhängigkeiten
+# Für dieses Skript müssen folgende Programme bereits installiert und konfiguriert sein.
+# - doas
