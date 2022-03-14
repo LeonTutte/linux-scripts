d4b0c12a6463d2eb69b990cb3bd425f4c2beb8ec
Leon Tutte <leonacc@ltutte.de>
2022-03-14 16:09:53 +0100

    Worked on postgresql
    - Script is not done yet

--- Ubuntu/install-postgresql.sh
+++ Ubuntu/install-postgresql.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # Ubuntu PostgreSQL Installation
 
+#Installation
 sudo apt update --force-yes
 sudo apt upgrade --force-yes
 sudo apt install postgresql postgresql-contrib --force-yes
 sudo apt autoremove --force-yes
+
+#Configuration
+sudo -i -u postgres
+createuser --interactive
+
+#network access
+cd /etc/postgresql/12/main
+echo
+
+#Firewall
+sudo ufw allow 5432/tcp
